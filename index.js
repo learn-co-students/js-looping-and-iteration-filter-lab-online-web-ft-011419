17de843ff14a1d0eced71d6c4559361e3c6b5c9e
Sim Greenbaum <gbs4ever2@gmail.com>
2019-04-24 03:55:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file
+function findMatching(driver, string){
+return driver.filter(function (name) { return name.toLowerCase() === string.toLowerCase() ; })
+}
+function  fuzzyMatch(driver,string){
+const match = string.length
+return driver.filter(function (elment)   {const first = elment.slice(0,match);  return first === string })
+
+
+}
+function matchName(driver, string){
+return driver.filter(function (object) { return object.name.toLowerCase() === string.toLowerCase() ; })
+}
