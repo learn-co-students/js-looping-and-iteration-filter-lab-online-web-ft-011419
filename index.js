34f94f900ab52dcdde987c7c18ee384db5c0eb8e
Mina Mekhael <mina.mekhael@ymail.com>
2019-05-04 17:12:17 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution in this file
+function findMatching (array, name) {
+  return array.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+}
+
+ function fuzzyMatch (array, partialName) {
+  let lengthOfName = partialName.length;
+  return array.filter(function (driverName) {
+    return driverName.slice(0, lengthOfName) === partialName;
+  });
+}
+
+ function matchName (array, name) {
+  return array.filter(function (driver) {
+    return driver.name.toLowerCase() === name.toLowerCase();
+  });
+}
